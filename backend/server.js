92fd75ffc07a81a5734f9dda3815e19ecf1dee13
yas-amini <yaasamin.amini@gmail.com>
2026-01-10 16:30:41 +0100

    Add Express server with API endpoints and nodemon dev script

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 const PORT = 3000;
@@ -11,9 +12,32 @@ app.use(express.json());
 
 // Test route
 app.get('/', (req, res) => {
-  res.send('Freaky Fashion API is running!');
+  res.send('Freaky Fashion API is on!');
 });
 
+// GET all products
+app.get('/api/products', (req, res) => {
+  const products = db.prepare('SELECT * FROM products').all();
+  res.json(products);
+});
+
+// GET single product by slug
+app.get('/api/products/:slug', (req, res) => {
+  const product = db.prepare('SELECT * FROM products WHERE slug = ?').get(req.params.slug);
+  
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+  
+  res.json(product);
+});
+
+// Search products by name
+app.get('/api/search', (req, res) => {
+  const query = req.query.q || '';
+  const products = db.prepare('SELECT * FROM products WHERE name LIKE ?').all(`%${query}%`);
+  res.json(products);
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
